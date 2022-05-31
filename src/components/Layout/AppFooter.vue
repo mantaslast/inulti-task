0ab251462dd19ee3da308a15bb4ac366353d85c4
Mantas Lastakauskas <mantaslastakauskas@wixs-MBP.zyxel.com>
2022-05-31 22:48:33 +0300

    tailwind css setup

--- src/components/Layout/AppFooter.vue
+++ src/components/Layout/AppFooter.vue
@@ -1,12 +1,17 @@
 <template functional>
-  <footer>
-    <nav>
-      <ul class="flex">
-        <li>About Us</li>
-        <li>Privacy Policy</li>
-        <li>Terms Of Use</li>
-      </ul>
-    </nav>
+  <footer
+    class="w-full font-['Open_Sans'] text-gray-400 pb-[19px] sm:pb-[54px] mt-auto"
+  >
+    <div class="flex flex-col items-center mx-auto max-w-md">
+      <nav class="w-full pb-5">
+        <ul class="flex justify-center text-sm">
+          <li>About Us</li>
+          <li class="px-[25px] sm:px-[30px]">Privacy Policy</li>
+          <li>Terms Of Use</li>
+        </ul>
+      </nav>
+      <p class="text-xs">&copy; 2021 All Rights Reserved.</p>
+    </div>
   </footer>
 </template>
 
